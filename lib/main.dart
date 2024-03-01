f5474fc1bd832aa51f6b40be74436f1303a25f71
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2024-03-01 18:11:52 +0700

    [Satria][BLOC] - init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:unishare/app/modules/splashscreen/views/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Memastikan bahwa flutter sudah terinisialisasi
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MaterialApp(
     home: Scaffold(
       body: SplashScreen(
