c5b42e18a0dbc8f285316c266b9e2cd1dfb1672e
rasyeeed <muhammadnurrasyid@gmail.com>
2024-04-02 09:46:44 +0700

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:unishare/app/modules/admin/makebeasiswapost.dart';
